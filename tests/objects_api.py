ade8cba27e58307ce03380ac6a57a679f981d6bc
shikhasinghal <shikha040@gmail.com>
2025-10-10 13:34:41 +0530

    added negative etst cases for patch API

--- tests/objects_api.py
+++ tests/objects_api.py
@@ -1,22 +1,28 @@
 import pytest
 import requests
 
+# Test to fetch all the items in the API
 def test_list_of_all_objects(api_url_get):
     data = api_url_get
     for object in data:
+        # validate that the data in each item is not empty
         assert(object.keys() is not None)
         assert(object['id'] is not None)
 
 
+# test to validate that API is rendering single item at a time
 @pytest.mark.parametrize('id', (3, 5))
 def test_single_object_with_id(id):
     params = {'id': id}
     response = requests.get("https://api.restful-api.dev/objects", params=params)
     response.raise_for_status()
     data = response.json()
+    # asserting that the rendered item is same as the one given in the request
     assert(int(data[0]['id']) == id)
+    assert (data[0].keys() is not None)
 
 
+# test to validate that the API is returning multiple objects as requested
 @pytest.mark.parametrize('id', [('3','5')])
 def test_multiple_objects_with_ids(id):
     params = {'id': id}
@@ -25,11 +31,16 @@ def test_multiple_objects_with_ids(id):
     data = response.json()
 
     assert(len(data) == len(id))
+    # asserting that the rendered items are same as the one given in the request
     for i in range(len(id)):
         assert(data[i]['id'] == id[i])
 
+
+# Test to validate that an object can be added to the API
 @pytest.mark.parametrize('id', [3,5])
 def test_add_object(api_url_get, id):
+
+    # define the JSON for the object to be addded
     data = {
        "name": "Apple MacBook Pro 16",
        "data": {
@@ -46,6 +57,7 @@ def test_add_object(api_url_get, id):
 
     data = api_url_get
 
+    # Validate the API that the added object is returned
     for object in data:
         if object['id'] == 7:
             assert(object['name'] == 'Apple MacBook Pro 16')
@@ -55,6 +67,8 @@ def test_add_object(api_url_get, id):
             assert(object['data']['Hard disk size'] == '1 TB')
             break
 
+
+# test to validate that the object can be modified
 @pytest.mark.xfail
 def test_patch_object(api_url_get):
     data = {
@@ -69,6 +83,7 @@ def test_patch_object(api_url_get):
     if response.status_code == 200:
         data = api_url_get
 
+        # validating that the modified object is returning the data as given in the request
         for object in data:
             if object['id'] == 7:
                 assert (object['name'] == 'Apple MacBook Pro 16 (Updated Name)')
--- tests/objects_api_negative_tests.py
+++ tests/objects_api_negative_tests.py
@@ -24,8 +24,47 @@ def test_invalid_parameter(id):
 # Here the API should throw proper error message with 400 status code that the parameter give is invalid.
 # its accepting any random value fpr any parameter.
 
+# Test to validate that invalid body for the patch request is not allowed
+def test_patch_object_invalid_body(api_url_get):
+    data = {
+        "description": "Apple MacBook Pro 16 (Updated Name)",
+        "age": "error"
+    }
+
+    # Make a Patch request to an API endpoint
+    response = requests.put("https://api.restful-api.dev/objects", json=data, timeout=30)
+    response.raise_for_status()
+
+    # Check the status code which should be 400 for bad request as the body has some unwanted fields
+    assert response.status_code == 400
 
 
+
+# Test to validate that empty body for the patch request is not allowed
+def test_patch_object_empty_body(api_url_get):
+    data = {
+    }
+
+    # Make a Patch request to an API endpoint
+    response = requests.put("https://api.restful-api.dev/objects", json=data, timeout=30)
+    response.raise_for_status()
+
+    # Check the status code which should be 400 for bad request as the body is empty
+    assert response.status_code == 400
+
+
+# Test to validate that the body structure is malformed
+def test_patch_object_malformed_body(api_url_get):
+    data = {
+        ("name"), ("age")
+    }
+
+    # Make a Patch request to an API endpoint
+    response = requests.put("https://api.restful-api.dev/objects", json=data, timeout=30)
+    response.raise_for_status()
+
+    # Check the status code which should be 400 for bad request as the body is not a proper json
+    assert response.status_code == 400
 
 
 
